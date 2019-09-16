a08bfd5a5b6283d85152c0cccb80064af901d2e8
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-09-16 13:42:46 -0500

    more reports working

--- report.py
+++ report.py
@@ -11,6 +11,23 @@ class Student():
     def __repr__(self):
         return f'{self.first_name} {self.last_name} is in {self.cohort}'
 
+class Cohort():
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'{self.name}'
+
+class Exercise():
+
+    def __init__(self, name, language):
+        self.name = name
+        self.language = language
+
+    def __repr__(self):
+        return f'{self.name} is a {self.language} exercise'
+
 class StudentExerciseReports():
 
     """Methods for reports on the Student Exercises database"""
@@ -18,14 +35,15 @@ class StudentExerciseReports():
     def __init__(self):
         self.db_path = "/Users/user/workspace/python/StudentExercises/studentexercises.db"
 
-    def create_student(self, cursor, row):
-        return Student(row[1], row[2], row[3], row[5])
+    # def create_student(self, cursor, row):
+    #     return Student(row[1], row[2], row[3], row[5])
 
     def all_students(self):
 
         """Retrieve all students with the cohort name"""
 
         with sqlite3.connect(self.db_path) as conn:
+            # using lambda:
             conn.row_factory = lambda cursor, row: Student(
     row[1], row[2], row[3], row[5]
 )
@@ -48,6 +66,57 @@ class StudentExerciseReports():
 
             for student in all_students:
                 print(student)
+    
+    
+    def all_cohorts(self):
+
+        """Retrieve all cohort names"""
+
+        with sqlite3.connect(self.db_path) as conn:
+            # using lambda:
+            conn.row_factory = lambda cursor, row: Cohort(
+    row[1]
+)
+            db_cursor = conn.cursor()
+
+
+            db_cursor.execute("""
+            select c.Id,
+                c.Name
+            from Cohort c
+            """)
+
+            all_cohorts = db_cursor.fetchall()
+
+            for cohort in all_cohorts:
+                print(cohort)
+    
+    
+    def all_exercises(self):
+
+        """Retrieve all cohort names"""
+
+        with sqlite3.connect(self.db_path) as conn:
+            # using lambda:
+            conn.row_factory = lambda cursor, row: Exercise(
+    row[1], row[2]
+)
+            db_cursor = conn.cursor()
+
+
+            db_cursor.execute("""
+            select e.Id,
+                e.Name,
+                e.Language
+            from Exercise e
+            """)
+
+            all_exercises = db_cursor.fetchall()
+
+            for exercise in all_exercises:
+                print(exercise)
 
 reports = StudentExerciseReports()
-reports.all_students()
+# reports.all_students()
+# reports.all_cohorts()
+reports.all_exercises()
